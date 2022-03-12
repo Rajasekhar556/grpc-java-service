63631a3d8e362d863a456f22ac3a7b2e39cdc828
Polavarapu Rajasekhar <Rajasekhar.Rajasekhar@icims.com>
2022-03-12 20:11:27 +0530

    initial commit

---
+++ grpc-java-service/src/main/java/com/org/grpc/grpcjavaservice/GrpcJavaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.org.grpc.grpcjavaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcJavaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcJavaServiceApplication.class, args);
+	}
+
+}
---
+++ grpc-java-service/src/test/java/com/org/grpc/grpcjavaservice/GrpcJavaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.grpc.grpcjavaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcJavaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
